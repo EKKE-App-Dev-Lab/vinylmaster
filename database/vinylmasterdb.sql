d8f1b5be2b58c254419ee970cea57fd1adff7576
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-01-13 09:53:21 +0100

    adatbázis struktúra kidolgozása - products tábla létrehozása

--- database/vinylmasterdb.sql
+++ database/vinylmasterdb.sql
@@ -2,3 +2,15 @@ drop database if exists `vinylmasterdb`;
 create database if not exists `vinylmasterdb`;
 use `vinylmasterdb`;
 
+CREATE TABLE `products`
+(
+    `productID` bigint(20)   NOT NULL AUTO_INCREMENT,
+    `albumcim`  varchar(255) not null,
+    `eloado`    varchar(255) not null,
+    `leiras`    text         not null,
+    `allapot`   varchar(255) not null,
+    `kategoria` varchar(255) not null,
+    `ar`        int          not null,
+    `boritokep` varchar(255) null,
+    PRIMARY KEY (`productID`)
+);
