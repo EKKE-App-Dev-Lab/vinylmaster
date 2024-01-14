4aed141eac0204a842a8219bf6fb39e9a6231232
levente.retfalvi <retfalvi.levente@virgo.hu>
2024-01-14 19:44:22 +0100

    F-0005 - stílusok cart.php

--- cart.php
+++ cart.php
@@ -60,5 +60,11 @@ if(filter_input(INPUT_GET, 'action') == 'delete'){
             $('input[type="number"]').niceNumber();
         });
     </script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0e16635bd7.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 </head>
 </html>
