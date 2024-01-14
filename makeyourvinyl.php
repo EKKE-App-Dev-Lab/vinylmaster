664ba0501ff5945e879d961b343466addd498bf3
Bauer Andras <nimbrinumbri@outlook.com>
2024-01-15 00:53:47 +0100

    Készíts saját lemezt aloldalon a slide fotók méretének javítása, valamint a lábjegyzet javítása

--- makeyourvinyl.php
+++ makeyourvinyl.php
@@ -140,66 +140,13 @@ include "./AdditionalPHP/startSession.php";
         </section>
         <!--Különleges ajánlat vége-->
 
-<footer class="footer-group">
+        <!--Start Footer-->
+        <?php include './Includes/Footer.php';?>
+        <!--End Footer-->
 
-    <div class="footer">
-
-        <div class="logo">
-            <span class="logo-name">VINYLMASTER</span>
-        </div>
-
-        <div class="social-media">
-                    <span class="facebook">
-                        <a href=#><i class="fab fa-facebook-square"></i></a>
-                    </span>
-            <span class="twitter">
-                        <a href=#><i class="fab fa-twitter-square"></i></a>
-                    </span>
-            <span class="instagram">
-                        <a href=#><i class="fab fa-instagram-square"></i></a>
-                    </span>
-            <span class="pinterest">
-                        <a href=#><i class="fab fa-pinterest-square"></i></a>
-                    </span>
-        </div>
-
-        <hr size="2px" width="80%" color="white">
-        <hr size="2px" width="80%" color="white">
-
-        <div class="contact-links">
-            <span class="phone"><i class="fas fa-phone-square-alt"></i> +36 36 1234 XXXX</span>
-            <span class="address">Eger, Eszterházy tér 1, 3300 Magyarország</span>
-        </div>
-
-        <div class="legal-links">
-            <span class="privacy-policy"><b><a href=#>ADATKEZELÉSI TÁJÉKOZTATÓ</a></b></span>
-            <span class="term-of-use"><b><a href=#>FELHASZNÁLÓI FELTÉTELEK</a></b></span>
-        </div>
-
-        <div class="copyright">
-            <span class="copyright-text">&#169; 2024 Design by András & Erik & Levente & Tamás</span>
-        </div>
-
-    </div>
-
-</footer>
-
-
-<div class="bottom-nav-group">
-    <nav class="bottom-nav">
-        <a href="login.html" class="bottom-nav-link">
-            <i class="fas fa-user bottom-nav-icon" ></i>
-            <span class="bottom-nav-text">Fiók</span>
-        </a>
-        <a href="#" class="bottom-nav-link">
-            <i class="fas fa-search"></i>
-            <span class="bottom-nav-text">Keresés</span>
-        </a>
-        <a href="#" class="bottom-nav-link">
-            <i class="fas fa-shopping-cart"></i>
-            <span class="bottom-nav-text">Kosaram</span>
-        </a>
-    </nav>
-</div>
+        
+        <!-- Start Mobil Alsó Nav -->
+        <?php include './Includes/MobileBottomNav.php';?>
+        <!-- End Mobil Alsó Nav -->
 </body>
 </html>
