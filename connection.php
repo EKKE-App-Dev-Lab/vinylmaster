1390801c7828499fb988658ffc4d84c15c813645
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-01-14 13:16:19 +0100

    connection.php elkészítése az adatbázis kapcsolathoz

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "vinylmasterdb";
+$conn = mysqli_connect($host, $username, $pass, $dbname);
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
