62631adce10ce91baa24f0467b150767e4cb566a
levente.retfalvi <retfalvi.levente@virgo.hu>
2024-01-14 19:58:31 +0100

    F-0005 checkout.php

---
+++ checkout.php
@@ -0,0 +1,30 @@
+<?php
+define('Access', TRUE);
+
+include "./AdditionalPHP/startSession.php";
+include_once 'connection.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>VINYLMASTER | Pénztár</title>
+
+    <link href="checkout/bootstrap.min.css" rel="stylesheet">
+    <link href="checkout/form-validation.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+</div>
+
+
+<script src="checkout/bootstrap.bundle.min.js"></script>
+<script src="checkout/form-validation.js"></script>
+
+</body>
+</html>
