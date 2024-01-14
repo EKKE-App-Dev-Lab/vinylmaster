92cf915604eb67744a7c4d03d2269798e10c1315
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-01-14 16:28:16 +0100

    sql query a kategóriák lekérdezéséhez products.php -ban

---
+++ products.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="en-MU">
+<head>
+    <meta charset="utf-8">
+    <title>VINYLMASTER | Termékek</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php
+    include_once 'connection.php';
+    ?>
+
+</head>
+
+<body>
+
+
+<?php
+
+$Q_fetch_categories = "SELECT * FROM categories";
+
+?>
+
+
+<?php $page = 'products'?>
+
+</body>
+</html>
