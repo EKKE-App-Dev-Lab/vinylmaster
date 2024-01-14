7d521503c4fc8abdf3f40e979723c8194351812e
levente.retfalvi <retfalvi.levente@virgo.hu>
2024-01-14 20:06:07 +0100

    F-0005 checkout.php validatorok

--- checkout.php
+++ checkout.php
@@ -14,8 +14,116 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //KERESZTNÉV VALIDÁLÁSA
     $fname = test_input($_POST["fname"]);
+    // leellenőrzi, hogy csak betűket és spacet tartalmaz-e
     if (!preg_match("/^[a-zA-Z-' ]*$/", $fname)) {
         $fnameErr = "Csak betű és szóköz engedélyezett";
+    }
+
+    //VEZETÉKNÉV VALIDÁLÁSA
+    $lname = test_input($_POST["lname"]);
+    // leellenőrzi, hogy csak betűket és spacet tartalmaz-e
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $lname)) {
+        $lnameErr = "Csak betű és szóköz engedélyezett";
+    }
+
+    //EMAIL VALIDÁLÁSA
+    $email = test_input($_POST["email"]);
+    // ellenőrzi a formátumot
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Érvénytelen email cím formátum";
+    }
+
+    //CÍM VALIDÁLÁSA
+    $address = test_input($_POST["address_checkout"]);
+    // Ellenőrizze, hogy a cím számmal kezdődik-e, azt követik-e betűk, tartalmaz-e szóközt, kötőjelet és vesszőt.
+    if (!preg_match("/^[0-9a-zA-Z\s,-]+$/", $address)) {
+        $addressErr = "Érvénytelen cím";
+    }
+
+    //VÁROS VALIDÁLÁSA
+    $city = test_input($_POST["city"]);
+    // ellenőrzi, hogy city == options
+    if ($city == "Port Louis" || $city == "Curepipe" || $city == "Vacoas" || $city == "Quatre Bornes" || $city == "Rose Hill" || $city == "Flic En Flac" || $city == "Phoenix") {
+        //valid
+    } else {
+        $cityErr = "Érvénytelen város";
+    }
+
+    //IRÁNYÍTÓSZÁM VALIDÁLÁSA
+    $zip = test_input($_POST["zip"]);
+    // ellenőrzi, hogy az irányítószám pontosan 5 számjegyet tartalmaz.
+    if (!preg_match("/^[0-9]{5}/", $zip)) {
+        $zipErr = "Érvénytelen irányítószám";
+    }
+
+    //FIZETÉSI METÓDUS VALIDÁLÁSA
+    $paymentMethod = test_input($_POST["paymentMethod"]);
+
+    //REGEX KÁRTYATÍPUS ALAPJÁN
+    $cardtype = array(
+        "visa" => "/^4[0-9]{15}$/",
+        // "mastercard" => "/^5[1-5][0-9]{14}$/"
+
+    );
+
+    //HITELKÁRTYA NÉV ÉRVÉNYESSÉGELLENŐRZÉS
+    $ccname = test_input($_POST["ccname"]);
+    // Ellenőrzi, hogy a hitelkártya név csak betűket és szóközöket tartalmaz-e.
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $ccname)) {
+        $ccnameErr = "Csak betű és szóköz engedélyezett";
+    }
+
+    //HITELKÁRTYASZÁM VALIDÁLÁSA
+    $ccnum = test_input($_POST["ccnum"]);
+    // ellenprzi, hogy a hitelkártyaszám illeszkedik-e a kártyatípusnak megfelelő regexbe
+    if (!preg_match($cardtype['visa'], $ccnum)) {
+        $ccnumErr = "Érvénytelen kártyaszám
+          <br>
+          Elnézést! Jelenleg a rendszerünk a VISA és MasterCard kártyákat támogatja.";
+    }
+
+    //HITELKÁRTYA LEJÁRATI DÁTUM VALIDÁLÁSA
+    if (empty($_POST["ccexp_m"]) || empty($_POST["ccexp_y"])) {
+        $ccexpErr = "Kérlek add meg a lejárati dátumot";
+    } else {
+        $ccexp_m = test_input($_POST["ccexp_m"]);
+        $ccexp_y = test_input($_POST["ccexp_y"]);
+        //VALIDÁLJA A HÓNAPOT
+        if ((int)$ccexp_m > 0 && (int)$ccexp_m < 13) {
+            //VALIDÁLJA AZ ÉVET
+            if ((int)$ccexp_y > 1900 && (int)$ccexp_y < 4000) {
+                $expired_check = \DateTime::createFromFormat('my', $ccexp_m . $ccexp_y);
+                $month_now = date('m');
+                $year_now = date('y');
+                $now = new \DateTime();
+
+                //ELLENŐRZI, HOGY LEJÁRT-E A KÁRTYA
+                if ($year_now < (int)$ccexp_y) {
+                } else if ($year_now == (int)$ccexp_y) {
+                    if ($month_now < (int)$ccexp_m) {
+                    } else {
+                        $ccexpErr = "A kártya már lejárt";
+                    }
+                } else {
+                    $ccexpErr = "A kártya már lejárt.";
+                }
+            } else {
+                $ccexpErr = "Érvénytelen év";
+            }
+        } else {
+            $ccexpErr = "Érvénytelen lejárati dátum";
+        }
+    }
+
+
+    //HITELKÁRTYA CVV VALIDÁLÁSA
+    $cccvv = test_input($_POST["cccvv"]);
+    if (preg_match("/^[0-9]{3,4}$/", $cccvv)) {
+    } else {
+        $cccvvErr = "Érvénytelen CVV";
+    }
+    if ($fnameErr == "" && $lnameErr == "" && $emailErr == "" && $addressErr == "" && $countryErr == "" && $cityErr == "" && $zipErr == "" && $paymentMethodErr == "" && $ccnameErr == "" && $ccnumErr == "" && $ccexpErr == "" && $cccvvErr == "") {
+        $validated = true;
     }
 }
 
