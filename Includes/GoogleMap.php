1001e058a99d0e5dcc3424a2027ca42e75fd0db8
Bauer Andras <nimbrinumbri@outlook.com>
2024-01-14 23:24:16 +0100

    Google maps cím javítás

--- Includes/GoogleMap.php
+++ Includes/GoogleMap.php
@@ -6,5 +6,7 @@
     </div>
 
     <div class="google-map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2674.8768845064787!2d20.372904274442824!3d47.9000681712182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47408d9db7860baf%3A0x40e624316abf2113!2sEszterh%C3%A1zy%20K%C3%A1roly%20Katolikus%20Egyetem!5e0!3m2!1shu!2sat!4v1705095530212!5m2!1shu!2sat" width="100%" height="450" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4445.274923533052!2d20.384436052609672!3d47.90157274773578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47408d84f6c8bf51%3A0x80a9f6a0d2a20313!2sEszterh%C3%A1zy%20K%C3%A1roly%20Katolikus%20Egyetem%20C%20%C3%89p%C3%BClet!5e1!3m2!1shu!2shu!4v1705270824280!5m2!1shu!2shu" width="100%" height="450" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+        </iframe>
+    </div>
 </div>
