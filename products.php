1ddba518bada2ce19c9a1c0a70a34f22ddd63fce
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-01-14 16:38:11 +0100

    AJAX scriptek a az termékek fetch-eléséhez - kategóriák szerint, - sorba rendezve - típus szerint products.php -ban

--- products.php
+++ products.php
@@ -25,5 +25,122 @@ $Q_fetch_categories = "SELECT * FROM categories";
 
 <?php $page = 'products'?>
 
+
+
+    <script>
+
+        function display_products_by_cat_id(cat_id, cat_name){
+            var xhttp;
+
+            //AJAX
+            console.log('entered ajax');
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log('ready');
+                    document.getElementById("result").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "category_search.php?cat_id="+cat_id, true);
+            xhttp.send();
+            console.log('sent');
+
+            document.getElementById('small_title').innerHTML = 'KATEGÓRIA';
+            document.getElementById('big_title').innerHTML = cat_name;
+
+        }
+
+        function sortby_products(sort_num){
+            var xhttp;
+            //AJAX
+            console.log('entered ajax sortby');
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log('ready sortby');
+                    document.getElementById("result").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "sortby_search.php?sortby="+sort_num, true);
+            xhttp.send();
+            console.log('sent sortby');
+
+            document.getElementById('small_title').innerHTML = 'ÁR SZERINT RENDEZVE';
+
+            if(sort_num == 1){
+                document.getElementById('big_title').innerHTML = 'növekvő';
+            }
+            else if(sort_num ==2){
+                document.getElementById('big_title').innerHTML = 'csökkenő';
+            }
+
+        }
+
+        function display_products_by_type(p_type){
+            var xhttp;
+
+            console.log('entered ajax');
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log('ready');
+                    document.getElementById("result").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "products_by_type_search.php?p_type="+p_type, true);
+            xhttp.send();
+            console.log('sent');
+
+
+            if(p_type==1){
+                document.getElementById('small_title').innerHTML = 'új';
+            }
+            else  if(p_type==2){
+                document.getElementById('small_title').innerHTML = 'kiválasztott';
+            }
+            else  if(p_type==3){
+                document.getElementById('small_title').innerHTML = 'értékesítések';
+            }
+            else  if(p_type==4){
+                document.getElementById('small_title').innerHTML = 'best-seller';
+            }
+
+            document.getElementById('big_title').innerHTML = 'TERMÉKEK';
+
+        }
+
+
+        function display_products_by_type_second(p_type){
+            var xhttp;
+
+            console.log('entered ajax');
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log('ready');
+                    document.getElementById("result2").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "products_by_type_search.php?p_type="+p_type, true);
+            xhttp.send();
+            console.log('sent');
+
+            if(p_type==1){
+                document.getElementById('small_title2').innerHTML = 'új';
+            }
+            else  if(p_type==2){
+                document.getElementById('small_title2').innerHTML = 'kiválasztott';
+            }
+            else  if(p_type==3){
+                document.getElementById('small_title2').innerHTML = 'értékesítések';
+            }
+            else  if(p_type==4){
+                document.getElementById('small_title2').innerHTML = 'best-seller';
+            }
+            document.getElementById('big_title2').innerHTML = 'TERMÉKEK';
+        }
+        display_products_by_type(2);
+        display_products_by_type_second(1);
+    </script>
 </body>
 </html>
